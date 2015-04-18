abad6e82b02b5fd5f4afbdcf727f2d2f88a09647
Filip Benasic <filip.benasic@gmail.com>
2015-04-18 21:33:06 +0200

    Fixed memory leak caused by FXML loader

--- src/application/controller/MainAppController.java
+++ src/application/controller/MainAppController.java
@@ -1,49 +1,62 @@
 package application.controller;
 
 import application.DatabaseTools;
-import application.DbGen;
 import application.JDBC_Repository;
 import application.model.ColumnInfo;
 import javafx.beans.property.ReadOnlyStringWrapper;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeTableColumn;
-import javafx.scene.control.TreeTableColumn.CellDataFeatures;
 import javafx.scene.control.TreeTableView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
-import javafx.util.Callback;
 
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class MainAppController {
+
     @FXML
     private TreeTableView<ColumnInfo> columnInfoTreeTableView;
-    
+
     @FXML
     private BorderPane mainBorderPane;
-    
+
     private ObservableList<ColumnInfo> columnInfoList = null;
 
-    
+    private final FXMLLoader stringLoader = new FXMLLoader();
+    private final FXMLLoader integerLoader = new FXMLLoader();
+    private AnchorPane stringGeneratorSubScene;
+    private AnchorPane integerGeneratorSubScene;
+    private StringGeneratorController stringGeneratorController;
+    private IntegerGeneratorController integerGeneratorController;
+
+
     @FXML
     private void initialize() {
-
-
+        stringLoader.setLocation(getClass().getResource("../view/StringGenerator.fxml"));
+        integerLoader.setLocation(getClass().getResource("../view/IntegerGenerator.fxml"));
+        try {
+            stringGeneratorSubScene = stringLoader.load();
+            integerGeneratorSubScene = integerLoader.load();
+            stringGeneratorController = stringLoader.getController();
+            integerGeneratorController = integerLoader.getController();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-    
+
     public void init(){
         getTableInfoData();
         fillTableInfoTreeTableView();
     }
-    
+
     private void getTableInfoData(){
         DatabaseTools dt = new DatabaseTools(JDBC_Repository.getInstance().getconnectionInfo().getConnectionString());
         try {
@@ -53,96 +66,72 @@ public class MainAppController {
             e.printStackTrace();
         }
     }
-    
+
     private void fillTableInfoTreeTableView(){
-        
+
         Map<String, TreeItem<ColumnInfo>> roots = new HashMap<>();
         for (ColumnInfo columnInfo : columnInfoList) {
-            TreeItem<ColumnInfo> tempRoot = null;
+            TreeItem<ColumnInfo> tempRoot;
             if(roots.keySet().contains(columnInfo.getTableName())){
                 tempRoot = roots.get(columnInfo.getTableName());
                 tempRoot.getChildren().add(new TreeItem<>(columnInfo));
             }
             else{
                 ImageView tableIcon = new ImageView (new Image(getClass().getResourceAsStream("../view/tables1.png")));
-                tempRoot = new TreeItem<ColumnInfo>(new ColumnInfo(columnInfo.getTableName(), "", "", ""), tableIcon);
+                tempRoot = new TreeItem<>(new ColumnInfo(columnInfo.getTableName(), "", "", ""), tableIcon);
                 tempRoot.setExpanded(false);
-                tempRoot.getChildren().add(new TreeItem<ColumnInfo>(columnInfo));
+                tempRoot.getChildren().add(new TreeItem<>(columnInfo));
                 roots.put(columnInfo.getTableName(), tempRoot);
             }
         }
-        
-        TreeItem<ColumnInfo> mainRoot = new TreeItem<ColumnInfo>(new ColumnInfo("root", "", "", ""));
+
+        TreeItem<ColumnInfo> mainRoot = new TreeItem<>(new ColumnInfo("root", "", "", ""));
         mainRoot.setExpanded(true);
         for (TreeItem<ColumnInfo> root : roots.values()) {
             mainRoot.getChildren().add(root);
         }
-        
+
         TreeTableColumn<ColumnInfo, String> columnName = new TreeTableColumn<>("Column Name");
         TreeTableColumn<ColumnInfo, String> columnType = new TreeTableColumn<>("Column Type");
         columnName.setPrefWidth(150);
         columnType.setPrefWidth(150);
         columnInfoTreeTableView.getColumns().add(columnName);
         columnInfoTreeTableView.getColumns().add(columnType);
-        
-        columnName.setCellValueFactory(new Callback<CellDataFeatures<ColumnInfo, String>, ObservableValue<String>>() {
-            @Override
-            public ObservableValue<String> call(CellDataFeatures<ColumnInfo, String> p) {
-                ColumnInfo columnInfo = p.getValue().getValue();
-                if (columnInfo.getColumnSize().equals("")) {
-                    return new ReadOnlyStringWrapper(columnInfo.getTableName());
-                } else {
-                    return new ReadOnlyStringWrapper(columnInfo.getColumnName());
-                }
+
+        columnName.setCellValueFactory(p -> {
+            ColumnInfo columnInfo = p.getValue().getValue();
+            if (columnInfo.getColumnSize().equals("")) {
+                return new ReadOnlyStringWrapper(columnInfo.getTableName());
+            } else {
+                return new ReadOnlyStringWrapper(columnInfo.getColumnName());
             }
         });
-        columnType.setCellValueFactory(new Callback<CellDataFeatures<ColumnInfo, String>, ObservableValue<String>>() {
-            public ObservableValue<String> call(CellDataFeatures<ColumnInfo, String> p) {
-                ColumnInfo columnInfo = p.getValue().getValue();
-                if(columnInfo.getColumnSize().equals("")){
-                    return new ReadOnlyStringWrapper("");
-                }
-                else{
-                    return new ReadOnlyStringWrapper(columnInfo.getColumnType() + " [" + columnInfo.getColumnSize() + "]");
-                }
+        columnType.setCellValueFactory(p -> {
+            ColumnInfo columnInfo = p.getValue().getValue();
+            if(columnInfo.getColumnSize().equals("")){
+                return new ReadOnlyStringWrapper("");
+            }
+            else{
+                return new ReadOnlyStringWrapper(columnInfo.getColumnType() + " [" + columnInfo.getColumnSize() + "]");
             }
         });
         columnInfoTreeTableView.setRoot(mainRoot);
         columnInfoTreeTableView.setShowRoot(false);
-        
+
         columnInfoTreeTableView.getSelectionModel().selectedItemProperty()
             .addListener((observable, oldValue, newValue) -> {
-                FXMLLoader loader = new FXMLLoader();
-                AnchorPane stringGeneratorSubScene = null;
                 switch (newValue.getValue().getColumnType()) {
                     case "VARCHAR":
-                        loader.setLocation(DbGen.class.getResource("view/StringGenerator.fxml"));
-                        try {
-                            stringGeneratorSubScene = loader.load();
-                            StringGeneratorController stringGeneratorController = loader.getController();
-                            stringGeneratorController.setGenerator(newValue.getValue().getGenerator());
-                            mainBorderPane.setCenter(stringGeneratorSubScene);
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
+                        stringGeneratorController.setGenerator(newValue.getValue().getGenerator());
+                        mainBorderPane.setCenter(stringGeneratorSubScene);
                         break;
                     case "INTEGER":
-                        loader.setLocation(DbGen.class.getResource("view/IntegerGenerator.fxml"));
-                        try {
-                            stringGeneratorSubScene = loader.load();
-                            IntegerGeneratorController integerGeneratorController = loader.getController();
-                            integerGeneratorController.setGenerator(newValue.getValue().getGenerator());
-                            mainBorderPane.setCenter(stringGeneratorSubScene);
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
+                        integerGeneratorController.setGenerator(newValue.getValue().getGenerator());
+                        mainBorderPane.setCenter(integerGeneratorSubScene);
                         break;
                     default:
                         break;
                 }
-
         });
-        
     }
-
 }
