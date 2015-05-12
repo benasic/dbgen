e48a9f900587bf5bd4467cc89374846c9b0ea66b
Filip Benasic <filip.benasic@gmail.com>
2015-05-12 23:34:08 +0200

    Added first tooltip

--- src/application/controller/NumberGeneratorController.java
+++ src/application/controller/NumberGeneratorController.java
@@ -5,9 +5,12 @@ import application.generator.IntegerGenerator;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.geometry.Point2D;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Tooltip;
+import javafx.scene.layout.AnchorPane;
 import javafx.util.converter.NumberStringConverter;
 
 import java.text.NumberFormat;
@@ -15,6 +18,9 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class NumberGeneratorController {
+
+    @FXML
+    private AnchorPane anchorPane;
 
     @FXML
     private TabPane numberTabPane;
@@ -32,6 +38,8 @@ public class NumberGeneratorController {
 
     private IntegerGenerator integerGenerator;
 
+    private Tooltip minIntegerTooltip;
+
     // integer part
     @FXML
     private TextField integerMinNumberTextField;
@@ -44,14 +52,21 @@ public class NumberGeneratorController {
         public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
             Matcher m = p.matcher(newValue);
             if (m.find()) {
-                integerMinNumberTextField.textProperty().setValue(m.replaceAll(""));
+                //integerMinNumberTextField.textProperty().setValue(m.replaceAll(""));
+                Point2D point2D = integerMinNumberTextField.localToScreen(integerMinNumberTextField.getLayoutBounds().getMaxX(), integerMinNumberTextField.getLayoutBounds().getMaxY());
+                minIntegerTooltip.show(integerMinNumberTextField, point2D.getX(), point2D.getY());
+            }
+            else{
+                minIntegerTooltip.hide();
             }
         }
     };
 
     @FXML
     private void initialize(){
-
+        minIntegerTooltip = new Tooltip("Invalid integer value");
+        minIntegerTooltip.setAutoHide(false);
+        minIntegerTooltip.getStyleClass().add("ttip");
     }
 
     public void unbindValues(Generator generator, String type){
@@ -67,16 +82,19 @@ public class NumberGeneratorController {
         activeGeneratorType = type;
         switch (type){
             case "INTEGER":
+                numberTabPane.selectionModelProperty().get().select(integerTab);
                 integerTab.disableProperty().setValue(false);
                 smallIntTab.disableProperty().setValue(true);
                 tinyIntTab.disableProperty().setValue(true);
                 break;
             case "SMALLINT":
+                numberTabPane.selectionModelProperty().get().select(smallIntTab);
                 integerTab.disableProperty().setValue(true);
                 smallIntTab.disableProperty().setValue(false);
                 tinyIntTab.disableProperty().setValue(true);
                 break;
             case "TINYINT":
+                numberTabPane.selectionModelProperty().get().select(tinyIntTab);
                 integerTab.disableProperty().setValue(true);
                 smallIntTab.disableProperty().setValue(true);
                 tinyIntTab.disableProperty().setValue(false);
